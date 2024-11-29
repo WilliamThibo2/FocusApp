dbc18bbe9fcff024146da81f551941e7d20fcddd
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-29 10:01:56 -0500

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,7 @@
-const mongoose = require('mongoose');
-
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   completed: { type: Boolean, default: false },
+  priority: { type: String, enum: ['High', 'Medium', 'Low'], default: 'Medium' },
   createdAt: { type: Date, default: Date.now },
 });
 
